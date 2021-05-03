c63c4e6d9dbc9deacc3c832e254c54585a9f2e80
Aqlan-94 <79816323+Aqlan-94@users.noreply.github.com>
2021-05-03 11:57:23 +0800

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,22 @@
+
+document.addEventListener ('DOMContentLoaded' , function(event) {
+    document.getElementById("1").placeholder = "First Name ";
+    document.getElementById("2").placeholder = "Middle Name ";
+    document.getElementById("3").placeholder = "Last Name ";
+    document.getElementById("4").placeholder = "Nationality ";
+    document.getElementById("5").placeholder = "A resident of ";
+     document.getElementById("6").placeholder = "City ";
+     document.getElementById("7").placeholder = "Address ";
+    document.getElementById("8").placeholder = "Experiences";
+    document.getElementById("9").placeholder = "Qualification ";
+    document.getElementById("10").placeholder = "cell phone ";
+    document.getElementById("11").placeholder = "E-mail";
+   
+
+});
+  
+
+function Submit() {
+    alert("We have received your application.\nPlease stand-by for our our response.\nThank you! ");
+  
+  }
